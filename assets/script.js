96de1afa263199892e21d9e28c6ed01b48c416aa
johndean3326 <113262365+johndean3326@users.noreply.github.com>
2023-03-20 20:01:11 -0600

    initial commit
    site build

---
+++ assets/script.js
@@ -0,0 +1,49 @@
+var questionsList = [
+    {
+        question:"How many time zones are there in Russia",
+        anwsers:[
+            "4",
+            "9",
+            "2",
+            "11"
+        ],
+        correct: "11",
+    },
+ 
+    {
+         question:"What is the national flower of Japan?",
+         anwsers:[
+             "cherry blossom",
+             "lotus flower",
+             "orange bud",
+             "peace lilly"
+         ], 
+         correct: "cherry blossom",
+     },
+ 
+     {
+         question:"What country has the most islands in the world?",
+         anwsers:[
+             "japan",
+             "sweden",
+             "philippines"
+             "usa"
+         ],
+         correct: "sweden, 220,000",
+     },
+ 
+     {
+         question:"What year was the world wide web created",
+         anwsers:[
+             "1989",
+             "1991",
+             "1990",
+             "1988"
+         ],
+         correct: "1990",
+     }
+ 
+ ];
+ 
+
+startBtn.addEventListener('click', startQuiz);
